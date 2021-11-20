3182db4aee1ce743e88f8ec04ac638e4d7407dfc
HilbertWeb01 <syshilsoftware450@gmail.com>
2021-11-20 13:49:00 -0300

    Basic static components

---
+++ frontend/src/componets/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/HilbertWeb01" target="_blank" rel="noreferrer">Hilbert Muniz</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    SyS HiL Software: <a href="https://instagram.com/hilbertmunizde" target="_blank" rel="noreferrer">hilbertmunizde</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;

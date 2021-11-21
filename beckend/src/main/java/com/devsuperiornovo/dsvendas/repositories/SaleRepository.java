63e822a9dfdc134d8c30247a36a0b330af9b5ebc
HilbertWeb01 <syshilsoftware450@gmail.com>
2021-11-21 16:42:28 -0300

    Pagination

---
+++ beckend/src/main/java/com/devsuperiornovo/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperiornovo.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperiornovo.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository <Sale, Long>{
+
+}
